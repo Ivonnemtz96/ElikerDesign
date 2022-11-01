2ef91d0c9ba4a0165e9b8178d9f700eb9d14fe75
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-01 16:48:44 -0700

    se agrga lista en nuestros servicios

--- elikerdesign/modules/index.php
+++ elikerdesign/modules/index.php
@@ -361,6 +361,35 @@
     </div>
 </section>
 
+<section class="servicios">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h3>Our Services include:</h3>
+                <div class="row g-2">
+                    <div class="col-6">
+                        <p>
+                        <ul>
+                            <li><i class="checked"></i> Baeeeby seat</li> <br>
+                            <li>Car seat</li><br>
+                            <li>Boster seat</li><br>
+                            <li>Bilingual driver</li>
+                        </ul>
+                        </p>
+                    </div>
+                    <div class="col-6">
+                        <ul>
+                            <li>Travel insurance</li><br>
+                            <li>Turnpike</li><br>
+                            <li>Water and beer</li><br>
+                            <li>Grocery stop additional cost</li><br>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <section class="about-us pb-6 pt-6" style="
         background-image: url(images/shape4.png);
