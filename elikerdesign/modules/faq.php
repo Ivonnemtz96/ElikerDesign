50d12c4a5062a6ade912cc3945d38e0404c2bcad
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-02 15:56:01 -0700

    archivo creado faq.php

---
+++ elikerdesign/modules/faq.php
@@ -0,0 +1,56 @@
+<!-- style="background-image: url(images/1.webp);background-size: contain; background-position: inherit; background-repeat: no-repeat" -->
+<section class="banner pb-0 overflow-hidden">
+    <div class="container-fluid">
+        <div class="banner-in">
+            <div class="fondo row align-items-center" style="height: 40rem;">
+                <div class="col-md-2"></div>
+                <div class="col-12 col-lg-4 col-md-8 col-sm-12 mb-4 col order-2 order-md-2">
+                    <div class="banner-content text-lg-start">
+                        <h3 class="theme mb-0" style="text-align:justify; color:#414141 !important;">Transportation in Cabo at an amazing price!</h3>
+                        <p class="mb-4">
+                            Customize your service according to the needs of the trip, in this way your savings will be
+                            greater, the transportation services we offer are luxurios and completely private, so that
+                            you are calm and you can enjoy the vacations you have always desired.
+                        </p>
+                        <div class="row justify-content-center align-items-center">
+                            <div class="col-6 col-md-6">
+                                <p>
+                                    <b>Phone</b> Los Cabos <br>
+                                    <a href="tel:011526241367171" style="color: #000;">011-52-624-136-7171</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 d-block d-md-none d-lg-none">
+                    <img src="images/05.jpg" alt="" />
+                </div>
+                <div class="container" style="position: absolute;z-index:-2;">
+                <img class="d-none d-md-block d-lg-block"  src="images/4.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- slider escritorio -->
+<section class="slider" style="background-color: #f6f7f8;">
+    <h3 class=" text-center" style="color: #414141;">Our fleet</h3>
+    <h6 class=" text-center" style="color: #414141;">Vehicles are subject to availability</h6>
+    <img src="/images/slider/13.jpg" alt="">
+    <br>
+</section>
+<!-- Clima -->
+<section class="clima">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-1 col-lg-1"></div>
+        <div class="col-4 col-lg-2">
+            <img src="images/logo.jpg" style="max-width:80%" alt="">
+        </div>
+        <div class="col-8 col-lg-8 text-center">
+            <a class="weatherwidget-io" href="https://forecast7.com/en/22d89n109d92/cabo-san-lucas/" data-label_1="LOS CABOS" data-label_2="WEATHER" data-font="Open Sans" data-theme="pure" data-accent="rgba(255, 255, 255, 0.02)" data-mooncolor="#ffffff" data-cloudfill="#ffffff">LOS CABOS WEATHER</a>
+        </div>
+        <div class="col-1 col-lg-1"></div>
+
+    </div>
+</section>
