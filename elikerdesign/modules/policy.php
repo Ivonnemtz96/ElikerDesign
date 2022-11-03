ab135b8f05c6ef8bae6ff319b8b5e9f4fa4b5f6f
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-03 16:29:08 -0700

    términos y condiciones OK

---
+++ elikerdesign/modules/policy.php
@@ -0,0 +1,40 @@
+<!-- style="background-image: url(images/1.webp);background-size: contain; background-position: inherit; background-repeat: no-repeat" -->
+<section class="banner pb-0 overflow-hidden" style="background-color: #f6f7f8;" >
+    <div class="container mt-5 mb-5" >
+        <div class="row">
+            <div class="col-md-6">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/X6PeRcRlEC0"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+            </div>
+            <div class="col-md-6">
+                <span style="color: #414141;">COVID 19</span>
+                <h3 style="color: #414141;">We take care of you and you take care of us!</h3>
+                <p>
+                    Every day we clean and disinfect all our vehicles, guaranteeing the well-being of all. Take care,
+                    use
+                    mask and a disinfecting gel, let's avoid getting infected!
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Clima -->
+<section class="clima">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-1 col-lg-1"></div>
+        <div class="col-4 col-lg-2">
+            <img src="images/logo.jpg" style="max-width:70%" alt="">
+        </div>
+        <div class="col-8 col-lg-8 text-center">
+            <a class="weatherwidget-io" href="https://forecast7.com/en/22d89n109d92/cabo-san-lucas/"
+                data-label_1="LOS CABOS" data-label_2="WEATHER" data-font="Open Sans" data-theme="pure"
+                data-accent="rgba(255, 255, 255, 0.02)" data-mooncolor="#ffffff" data-cloudfill="#ffffff">LOS CABOS
+                WEATHER</a>
+        </div>
+        <div class="col-1 col-lg-1"></div>
+
+    </div>
+</section>
---
+++ elikerdesign/modules/terms.php
@@ -0,0 +1,65 @@
+<!-- style="background-image: url(images/1.webp);background-size: contain; background-position: inherit; background-repeat: no-repeat" -->
+<section class="banner pb-0 overflow-hidden" style="background-color: #f6f7f8;">
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-md-10">
+                <h3 style="color: #414141;">Terms and Conditions</h3>
+                <span>Updated October 21, 2021</span>
+                <p>
+                    According to the <a href="terms-and-conditions.php" style="color: blue;">Terms and Conditions</a>,
+                    the customer must request the cancellation of the service via
+                    email, we will need the information such as: <br><br>
+                <ul>
+                    <li>* Name, Last name</li> <br>
+                    <li>* Email</li><br>
+                    <li>* Voucher number</li> <br>
+                    <li>* Copy of the payment</li><br>
+                    <li>* Cell phone number</li><br>
+                </ul>
+                </p>
+                <p>
+                    Please, before requesting cancellation note the following: <br><br>
+                <ul>
+                    <li>* Cancel 72 hours before the service, you will receive a 100%</li> <br>
+                    <li>* Cancel 48 hours before the service, you will receive a 50%</li><br>
+                    <li>* Cancel the same day or 24 hours before, does not apply for reimbursement due to expenses
+                        incurred for your arrival</li> <br><br>
+                </ul>
+
+                <b>We reserve the right to cancel the service and it is not applicable for refund if you do not show up
+                    at the scheduled time and do not notify possible changes,</b>
+                when this happens you will be notified and you will have to book the service again if required.
+                </p>
+                <h6 style="color: #414141;">Refunds</h6>
+                <p>
+                    After the cancellation is approved, we will proceed to make the refund to the account that made the
+                    payment at the time of booking, it may take a few days for the refund to appear on your statement. <br><br>
+                    
+                    By paying with PayPal, you can receive an instant refund within 24 hours. <br><br>
+
+                    Paying by credit or debit card the estimated time is 24 hours, however, in our system we make the
+                    refund and the issuing bank is the one who decides how long the operation will take, if you have any
+                    problem please contact your bank. <br><br>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Clima -->
+<section class="clima">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-1 col-lg-1"></div>
+        <div class="col-4 col-lg-2">
+            <img src="images/logo.jpg" style="max-width:70%" alt="">
+        </div>
+        <div class="col-8 col-lg-8 text-center">
+            <a class="weatherwidget-io" href="https://forecast7.com/en/22d89n109d92/cabo-san-lucas/"
+                data-label_1="LOS CABOS" data-label_2="WEATHER" data-font="Open Sans" data-theme="pure"
+                data-accent="rgba(255, 255, 255, 0.02)" data-mooncolor="#ffffff" data-cloudfill="#ffffff">LOS CABOS
+                WEATHER</a>
+        </div>
+        <div class="col-1 col-lg-1"></div>
+
+    </div>
+</section>
