a8644460bc1aedce416e5d846bd0caa696a23f45
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-03 12:47:45 -0700

    archivos, contact y covid creados

---
+++ elikerdesign/modules/covid.php
@@ -0,0 +1,112 @@
+<!-- style="background-image: url(images/1.webp);background-size: contain; background-position: inherit; background-repeat: no-repeat" -->
+<section class="banner pb-0 overflow-hidden">
+    <div class="container-fluid" style="padding: 0; margin: 0;">
+        <img class="d-none d-md-block" src="images/07.jpg" style="width: 100%;" alt="">
+        <img class="d-block d-md-none" src="images/08.jpg" style="width: 100%;" alt="">
+    </div>
+</section>
+
+<!-- About Us  -->
+<section class="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col order-2 order-md-1"></div>
+            <div class="col-12 col-md-8 col order-1 order-md-2">
+                <h3 style="text-align:justify; color:#414141 !important;">About us</h3>
+                <p style="text-align:justify; color:#414141 !important;">
+                    Thanks to the effort and dedication of the Eliker Transfer team, we have able to help many families
+                    on their
+                    trip to Los Cabos with our private transportation service. We are thankful to our customers for
+                    choosing us
+                    and we are very pleased to continuing be their transportation company!
+                </p><br>
+                <h6 style="text-align:justify; color:#414141 !important;">Private transportation, within everyone's
+                    reach!</h6>
+                <p style="text-align:justify; color:#414141 !important;">
+                    At first it was and idea on paper, later we founded Eliker Transfer in 2014 in the city of Cabo San
+                    Lucas
+                    B.C.S. México. Our goal is to offer a transportation service that adapts to each need of clients at
+                    an
+                    affordable price, we want them to enjoy the trip and forget about their worries.
+                </p>
+            </div>
+            <div class="col-md-2 col order-3 order-md-3"></div>
+        </div>
+    </div>
+</section>
+
+<!-- Our Services -->
+<section class="services" style="background-color: #f6f7f8;">
+    <div class="container">
+        <div class="row h-100 justify-content-center text-center">
+            <div style="box-shadow: 1px 1px 5px #414141;border: 1px solid #414141; background-color: #fff;" class="mt-3 col-8 col-md-8 col-lg-3 p-5">
+                <img src="images/icons/car.png" alt="image not found">
+                <h5 style="text-align:center; color:#414141 !important;" class="mt-2">Private Transportation</h5> <br>
+                <p style="text-align: justify;">
+                    From San José del Cabo International Airport (SJD) to: <br> <br>
+                <ul style="text-align: justify;">
+                    <li>* San José del Cabo</li> <br>
+                    <li>* Tourist Corridor</li><br>
+                    <li>* Cabo San Lucas</li><br>
+                    <li>* Pacific Area</li><br>
+                </ul>
+                </p><br>
+                <p style="text-align: justify;">
+                    <b>One way or Round Trip</b>
+                </p> <br> <br> <br>
+                <a class="btn-zona1 btn-lg mt-1" type="button"><i class="icon-envelope"></i> Contact us</a>
+            </div>
+            <div class="col-5 col-md-1"></div>
+            <div style="box-shadow: 1px 1px 5px #414141;border: 1px solid #414141; background-color: #fff;" class="mt-3 col-8 col-md-8 col-lg-3 p-5">
+                <img src="images/icons/map.png" alt="image not found">
+                <h5 style="text-align:center; color:#414141 !important;" class="mt-2">City Tours</h5> <br>
+                <p style="text-align: justify;">
+                    Private transportation from the hotel to the following cities: <br> <br>
+                <ul style="text-align: justify;">
+                    <li>* Todos Santos</li> <br>
+                    <li>* La Paz</li><br>
+                    <li>* Cabo San Lucas</li><br> <br>
+                </ul>
+                </p><br>
+                <p style="text-align: justify;">
+                    <b>Round Trip</b>
+                </p> <br><br>
+                <a class="btn-zona1 btn-lg mt-1" type="button"><i class="icon-envelope"></i> Contact us</a>
+            </div>
+            <div class="col-5 col-md-1"></div>
+            <div style="box-shadow: 1px 1px 5px #414141;border: 1px solid #414141; background-color: #fff;" class="mt-3 col-8 col-md-8 col-lg-3 p-5">
+                <img src="images/icons/events.png" alt="image not found">
+                <h5 style="text-align:center; color:#414141 !important;" class="mt-2">Special events</h5> <br>
+                <p style="text-align: justify;">
+                Private transportation from the hotel to the following things: <br> <br>
+                <ul style="text-align: justify;">
+                    <li>* Dinners</li> <br>
+                    <li>* Golf</li><br>
+                    <li>* Nightlife</li><br>
+                    <li>* Weddings and more</li><br><br>
+                </ul>
+                </p><br><br><br>
+                <a class="btn-zona1 btn-lg mt-2" type="button"><i class="icon-envelope"></i> Contact us</a>
+            </div>
+            <div class="col-5 col-md-1"></div>
+        </div>
+    </div>
+
+</section>
+<!-- Clima -->
+<section class="clima">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-1 col-lg-1"></div>
+        <div class="col-4 col-lg-2">
+            <img src="images/logo.jpg" style="max-width:80%" alt="">
+        </div>
+        <div class="col-8 col-lg-8 text-center">
+            <a class="weatherwidget-io" href="https://forecast7.com/en/22d89n109d92/cabo-san-lucas/"
+                data-label_1="LOS CABOS" data-label_2="WEATHER" data-font="Open Sans" data-theme="pure"
+                data-accent="rgba(255, 255, 255, 0.02)" data-mooncolor="#ffffff" data-cloudfill="#ffffff">LOS CABOS
+                WEATHER</a>
+        </div>
+        <div class="col-1 col-lg-1"></div>
+
+    </div>
+</section>
