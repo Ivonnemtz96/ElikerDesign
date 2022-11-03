c11a3cb0f9b225294ebc9544ab1c8ad7c50f4d42
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-11-03 10:46:43 -0700

    about-us archivo creado

---
+++ elikerdesign/modules/about-us.php
@@ -0,0 +1,31 @@
+<!-- style="background-image: url(images/1.webp);background-size: contain; background-position: inherit; background-repeat: no-repeat" -->
+<section class="banner pb-0 overflow-hidden">
+    <div class="container-fluid">
+        <img src="" alt="">
+    </div>
+</section>
+
+<!-- slider escritorio -->
+<section class="slider" style="background-color: #f6f7f8;">
+    <h3 class=" text-center" style="color: #414141;">Our fleet</h3>
+    <h6 class=" text-center" style="color: #414141;">Vehicles are subject to availability</h6>
+    <img style="width: 100%;" src="/images/slider/13.jpg" alt="">
+    <br>
+</section>
+<!-- Clima -->
+<section class="clima">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-1 col-lg-1"></div>
+        <div class="col-4 col-lg-2">
+            <img src="images/logo.jpg" style="max-width:80%" alt="">
+        </div>
+        <div class="col-8 col-lg-8 text-center">
+            <a class="weatherwidget-io" href="https://forecast7.com/en/22d89n109d92/cabo-san-lucas/"
+                data-label_1="LOS CABOS" data-label_2="WEATHER" data-font="Open Sans" data-theme="pure"
+                data-accent="rgba(255, 255, 255, 0.02)" data-mooncolor="#ffffff" data-cloudfill="#ffffff">LOS CABOS
+                WEATHER</a>
+        </div>
+        <div class="col-1 col-lg-1"></div>
+
+    </div>
+</section>
